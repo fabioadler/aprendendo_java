cf62bda2242194241833c32cda7f6882083eee8e
Fábio Adler de Luna Gomes <56208895+fabioadler@users.noreply.github.com>
2025-05-04 00:07:55 -0300

    Concluido com sucesso

--- calculadora/Calc.java
+++ calculadora/Calc.java
@@ -10,20 +10,39 @@ public class Calc {
 
         do {
 
-            System.out.println("Calculadora java");
+            System.out.println("\n\nCalculadora java\n");
             System.out.println("1) Soma");
             System.out.println("2) Subitração");
             System.out.println("3) Multiplicação");
             System.out.println("4) Divisão");
-            System.out.println("0) Sair");
+            System.out.println("0) Sair\n");
 
             opcao = scanner.nextInt();
 
             if(opcao >= 1 && opcao <= 4){
-                if(opcao == 1){}
-                else if(opcao == 2){}
-                else if(opcao == 3){}
-                else if(opcao == 4){}
+                System.out.print("Digite o primeiro numero: ");
+                double n1 = scanner.nextDouble();
+                System.out.print("Digite o Segundo numero: ");
+                double n2 = scanner.nextDouble();
+                double resultado = 0;
+                String op = " ? ";
+                if(opcao == 1){
+                    resultado = n1 + n2;
+                    op = " + ";
+                }
+                else if(opcao == 2){
+                    resultado = n1 - n2;
+                    op = " - ";
+                }
+                else if(opcao == 3){
+                    resultado = n1 * n2;
+                    op = " X ";
+                }
+                else if(opcao == 4){
+                    resultado = n1 / n2;
+                    op = " / ";
+                }
+                System.out.println("\n" + n1 + op + n2 + " = " + resultado);
             }
             else if(opcao != 0){
                 System.out.println("Opção invalida!");
